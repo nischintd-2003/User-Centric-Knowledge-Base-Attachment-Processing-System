1b46bedbdbaee865e36b570108d755f52df7b7dc
nischintd-2003 <nischintd@mindfiresolutions.com>
2026-01-22 13:08:54 +0530

    feat(middleware): add central error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import cors from 'cors';
+import { errorMiddleware } from './middleware/error-middleware';
 
 const app = express();
 
+app.use(cors());
+app.use(errorMiddleware);
 export default app;
