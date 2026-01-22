1c90e1251759f39db97f73258bc1065af9c72504
nischintd-2003 <nischintd@mindfiresolutions.com>
2026-01-22 17:48:28 +0530

    feat(routes):add collection routes

---
+++ src/controllers/collection-controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+
+export const createCollection = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
+
+export const getUserCollection = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
+
+export const updateCollection = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
+
+export const deleteCollection = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
--- src/routes/collection-route.ts
+++ src/routes/collection-route.ts
@@ -1,5 +1,16 @@
 import { Router } from 'express';
+import {
+  createCollection,
+  deleteCollection,
+  getUserCollection,
+  updateCollection,
+} from '../controllers/collection-controller';
 
 const collectionRoute = Router();
+
+collectionRoute.post('/', createCollection);
+collectionRoute.get('/', getUserCollection);
+collectionRoute.put('/:collectionId', updateCollection);
+collectionRoute.delete('/:collectionId', deleteCollection);
 
 export default collectionRoute;
