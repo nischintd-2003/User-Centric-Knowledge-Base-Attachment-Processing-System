6d7660d4baec0e39ff67ed6879285edf16a1accf
nischintd-2003 <nischintd@mindfiresolutions.com>
2026-01-23 12:49:11 +0530

    feat(model): add article model

--- src/interfaces/article.ts
+++ src/interfaces/article.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export interface IArticle extends Document {
   title: string;
-  content: number;
+  content: string;
   userId: mongoose.Types.ObjectId;
   collectionId: mongoose.Types.ObjectId;
   createdAt: Date;
---
+++ src/models/article-model.ts
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+import { IArticle } from '../interfaces/article';
+import User from './user-model';
+import Collection from './collection-model';
+import { title } from 'node:process';
+
+const articleSchema = new mongoose.Schema<IArticle>(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 100,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: User,
+      required: true,
+      index: true,
+    },
+    collectionId: {
+      type: mongoose.Types.ObjectId,
+      ref: Collection,
+      required: true,
+      index: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+articleSchema.index({ userId: 1, collectionId: 1, title: 1 }, { unique: true });
+
+const Article = mongoose.model('Article', articleSchema);
+
+export default Article;
