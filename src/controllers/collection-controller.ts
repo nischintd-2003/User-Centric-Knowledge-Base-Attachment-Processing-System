5f9524bf3360b2cd510fb336164f59662fe82fa1
nischintd-2003 <nischintd@mindfiresolutions.com>
2026-01-23 11:56:59 +0530

    feat(controller): add update collection controller

--- src/controllers/collection-controller.ts
+++ src/controllers/collection-controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import Collection from '../models/collection-model';
+import { ObjectId } from 'mongodb';
 
 interface IAuthRequest extends Request {
   user?: any;
@@ -39,12 +40,10 @@ export const getUserCollection = async (req: IAuthRequest, res: Response) => {
     }
 
     const userCollections = await Collection.find({ userId: userId }).sort({ createdAt: -1 });
-    res
-      .status(200)
-      .json({
-        message: 'Collection is created succefully',
-        body: { userCollection: userCollections },
-      });
+    res.status(200).json({
+      message: 'Collection is created succefully',
+      body: { userCollection: userCollections },
+    });
   } catch (error) {
     res.status(400).json({
       message: 'Error while fetching the collection',
@@ -53,12 +52,34 @@ export const getUserCollection = async (req: IAuthRequest, res: Response) => {
   }
 };
 
-export const updateCollection = async (req: Request, res: Response) => {
+export const updateCollection = async (req: IAuthRequest, res: Response) => {
   try {
+    const userId = req.user._id;
+    let { collectionId } = req.params;
+    const { name, description } = req.body;
+
+    if (Array.isArray(collectionId)) {
+      collectionId = collectionId[0];
+    }
+
+    const updatedCollection = await Collection.updateOne(
+      { userId: userId, _id: new ObjectId(collectionId) },
+      { $set: { name: name, description: description } },
+    );
+
+    res.status(200).json({
+      message: 'Collection is updated succefully',
+      body: { updatedCollection: updatedCollection },
+    });
+
+    if (userId) {
+      throw new Error('User not authenticated');
+    }
+    const documentToUpdate = await Collection.findOne({});
   } catch (error) {}
 };
 
-export const deleteCollection = async (req: Request, res: Response) => {
+export const deleteCollection = async (req: IAuthRequest, res: Response) => {
   try {
   } catch (error) {}
 };
