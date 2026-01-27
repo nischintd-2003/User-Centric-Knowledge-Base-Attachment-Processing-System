3deada5bab703577b613733c338d9e9b2500c93f
nischintd-2003 <nischintd@mindfiresolutions.com>
2026-01-27 16:12:04 +0530

    feat(documentation):setup swagger config file

---
+++ src/swagger/swagger.ts
@@ -0,0 +1,35 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'User-Centric Knowledge Base API',
+      version: '1.0.0',
+      description: 'Node.js Assignment 2 – Backend API Documentation',
+    },
+    servers: [
+      {
+        url: 'http://localhost:4000',
+        description: 'Local server',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ['./src/routes/*.ts'],
+};
+
+export const swaggerSpec = swaggerJsdoc(options);
